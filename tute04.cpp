eb29a98d2a7bbe911e275630017597a112222721
IT21176456 <99645165+it21176456@users.noreply.github.com>
2022-02-14 11:53:37 +0000

    what did you changed?

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,17 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  long fact=1;
+   for(int r=no;r>=1;r--)
+   {
+     fact=fact*r;
+   }
+
+   return fact;
+}
+long nCr(int n, int r)
+{
+  return Factorial(n)/(Factorial(r)*Factorial(n-r));
+}
